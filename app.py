7a0f9fc86c7ea3895717f6eb546cd9bc82751584
alperarslann <alper.arslan0505@gmail.com>
2024-03-16 02:15:01 +0300

    s

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def surerlik(sozcuk): # Türkçedeki sürerlik görünüşü var mı yok mu
     arama = re.search("..yor*", sozcuk)
     if arama:
         cikti = "Sürerlik görünüşü var." # True döndürüyor. 
-        #cikti = "Sürerlik görünüşü var."
+        #cikti = "Sürerlik görünüşü var." 
     return (cikti)
 
 def surerlilikSayac(tümceDizisi):
@@ -22,9 +22,6 @@ def surerlilikSayac(tümceDizisi):
                 surerlikEkleri.append(sozcukDegis.replace("yor", "YOR"))
     return surerlikEkleri
 
-
-
-            
 
 def calculate():
     tümce = entry.get()
@@ -52,33 +49,58 @@ def calculate():
         surerlilikSay = ("Sürerlik görünüşüyle çekimlenmiş eylem bulunamadı.").upper()
         
     result = f"Girdi: {tümce}\n" \
-            f"Liste: {liste}\n" \
-            f"Sürerlik Görünüşüyle Çekimlenmiş Eylem Var mı?: {surerlilikBul}\n" \
-            f"Sürerlik Görünüşüyle Çekimlenmiş Eylem(ler): {surerlilikSay}\n" \
-            f"Boşluk dahil Karakter Sayısı: {karakterSayisi}\n" \
-            f"Boşluksuz Karakter Sayısı: {kSayisi}\n" \
-            f"Sözcük Sayısı: {sozcukSayisi}\n" \
-            f"Sözcük Başına Ortalama Karakter Sayısı: {ortalamaKarakter}"
+        f"Liste: {liste}\n" \
+        f"Sürerlik Görünüşüyle Çekimlenmiş Eylem Var mı?: {surerlilikBul}\n" \
+        f"Sürerlik Görünüşüyle Çekimlenmiş Eylem(ler): {surerlilikSay}\n" \
+        f"Boşluk dahil Karakter Sayısı: {karakterSayisi}\n" \
+        f"Boşluksuz Karakter Sayısı: {kSayisi}\n" \
+        f"Sözcük Sayısı: {sozcukSayisi}\n" \
+        f"Sözcük Başına Ortalama Karakter Sayısı: {ortalamaKarakter}"
 
-    messagebox.showinfo("Sonuç", result)
-   
+    result_label = tk.Label(app, text=result, width=100, wraplength=500, justify="left", bg="lightgrey", font=("Arial", 10), relief="groove", padx=10, pady=10, anchor="w", borderwidth=2, highlightthickness=2, highlightbackground="black",)
+    result_label.pack()
+
     
+
+
+result_label = None  # Result_Label'ı global yapmak için                
+
+# önceki sonuçları temizleme fonksiyonu
+def clearAll():
+    for widget in app.winfo_children():
+        if isinstance(widget, tk.Label) and widget != result_label:
+            widget.destroy()
+        elif isinstance(widget, tk.Entry):
+            widget.delete(0, "end")
+            widget.insert(0, "Metin giriniz!")
+            widget.configure(fg="black")
+
 
 
 app = tk.Tk()
 app.title("Sözcük ve Karakter Uygulaması")
-app.geometry("500x250")
+app.geometry("1000x500")
 
 label = tk.Label(app, text="Sözcük veya Tümce Giriniz:")
 label.pack()
 
-entry = tk.Entry(app, width=50)
+entry = tk.Entry(app, width=50, borderwidth=2, highlightthickness=2, justify="center", font=("Arial", 10))
 entry.insert(0, "Metin giriniz!")
 entry.configure(fg="black")
+
+
 entry.bind("<FocusIn>", lambda args: entry.delete(0,"end"))
 entry.pack()
 
-button = tk.Button(app, text="Bul", command=calculate)
+button = tk.Button(app, text="Bul", bg="lightgrey", fg="black", command=calculate)
 button.pack(pady=10)
+app.bind('<Return>', lambda event: calculate()) # Enter tuşuna basıldığında da çalıştır
+
+
+silButton = tk.Button(app, text="Sonuçları Temizle", command=clearAll, bg="crimson")
+silButton.pack(pady=10)
+
+
+
 
 app.mainloop()
